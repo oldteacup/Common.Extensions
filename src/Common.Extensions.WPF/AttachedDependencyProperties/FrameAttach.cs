a8949f1e359b74416ca7a8821d882b0b041a4922
lenovo <lenovo@lenovo.com>
2022-10-28 14:48:28 +0800

    增加frame禁用后退的依赖属性

--- src/Common.Extensions.WPF/AttachedDependencyProperties/FrameAttach.cs
+++ src/Common.Extensions.WPF/AttachedDependencyProperties/FrameAttach.cs
@@ -2,8 +2,6 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -12,14 +10,27 @@ namespace Common.Extensions.WPF.AttachedDependencyProperties
 {
     public class FrameAttach
     {
+        /// <summary>
+        /// 是否启用Frame导航
+        /// </summary>
         public static readonly DependencyProperty IsEnabledFrameMapProperty;
         public static bool GetIsEnabledFrameMap(DependencyObject element) => (bool)element.GetValue(IsEnabledFrameMapProperty);
         public static void SetIsEnabledFrameMap(DependencyObject element, bool value) => element.SetCurrentValue(IsEnabledFrameMapProperty, value);
 
-
+        /// <summary>
+        /// 导航服务类关键字
+        /// </summary>
         public static readonly DependencyProperty NavigationServiceKeyProperty;
         public static string GetNavigationServiceKey(DependencyObject element) => (string)element.GetValue(NavigationServiceKeyProperty);
         public static void SetNavigationServiceKey(DependencyObject element, string value) => element.SetCurrentValue(NavigationServiceKeyProperty, value);
+
+        /// <summary>
+        /// 禁用Frame的Backspace响应
+        /// </summary>
+        public static readonly DependencyProperty DisableBackspaceProperty;
+        public static bool GetDisableBackspace(DependencyObject element) => (bool)element.GetValue(DisableBackspaceProperty);
+        public static void SetDisableBackspace(DependencyObject element, bool value) => element.SetCurrentValue(DisableBackspaceProperty, value);
+
 
         private static ConcurrentDictionary<string, Frame> _frames { get; } = new ConcurrentDictionary<string, Frame>();
 
@@ -27,6 +38,7 @@ namespace Common.Extensions.WPF.AttachedDependencyProperties
         {
             IsEnabledFrameMapProperty = DependencyProperty.RegisterAttached("IsEnabledFrameMap", typeof(bool), typeof(Frame), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.Inherits, IsEnabledNavigationServicePropertyChanged));
             NavigationServiceKeyProperty = DependencyProperty.RegisterAttached("NavigationServiceKey", typeof(string), typeof(System.Windows.Controls.Frame), new FrameworkPropertyMetadata(Guid.NewGuid().ToString(), FrameworkPropertyMetadataOptions.Inherits));
+            DisableBackspaceProperty = DependencyProperty.RegisterAttached("DisableBackspace", typeof(bool), typeof(Frame), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.Inherits, DisableBackspacePropertyChanged));
         }
 
         private static void IsEnabledNavigationServicePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -42,6 +54,27 @@ namespace Common.Extensions.WPF.AttachedDependencyProperties
             }
         }
 
+        private static void DisableBackspacePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Frame frame)
+            {
+                bool isDisableBackspace = GetDisableBackspace(d);
+                frame.JournalOwnership = System.Windows.Navigation.JournalOwnership.OwnsJournal;
+                frame.Navigated -= Frame_Navigated;
+                if (isDisableBackspace)
+                {
+                    frame.Navigated += Frame_Navigated;
+                }
+            }
+        }
+
+        private static void Frame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (sender is Frame frame)
+            {
+                frame.RemoveBackEntry();
+            }
+        }
 
         public static Frame? GetDefaultFrame()
         {
