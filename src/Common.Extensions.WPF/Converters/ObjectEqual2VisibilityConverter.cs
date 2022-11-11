b185c4e6779d6511af57f15eda88085c36b99693
lenovo <lenovo@lenovo.com>
2022-11-11 15:31:32 +0800

    新增转换器

---
+++ src/Common.Extensions.WPF/Converters/ObjectEqual2VisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Common.Extensions.WPF.Converters
+{
+    public class ObjectEqual2VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = true;
+            if (value != null)
+            {
+                result = value.Equals(parameter);
+            }
+            else if (parameter != null)
+            {
+                result = parameter.Equals(value);
+            }
+            return result ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
---
+++ src/Common.Extensions.WPF/Converters/ObjectEqual2VisibilityReConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Common.Extensions.WPF.Converters
+{
+    public class ObjectEqual2VisibilityReConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = false;
+            if (value != null)
+            {
+                result = !value.Equals(parameter);
+            }
+            if (parameter != null)
+            {
+                result = !parameter.Equals(value);
+            }
+            return result ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
