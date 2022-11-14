0ac69800f96a24c13c20fd4f86e56fa29811e4a9
lenovo <lenovo@lenovo.com>
2022-11-14 10:32:12 +0800

    新增visibility re converter 转换器

---
+++ src/Common.Extensions.WPF/Converters/VisibilityReConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Common.Extensions.WPF.Converters
+{
+    public class VisibilityReConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+            {
+                return visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return parameter ??= Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
